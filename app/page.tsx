6a86237a9051b00d31b1262a947eb52c00c86156
azzaazeel <azzaazeel@gmail.com>
2026-03-23 04:27:14 +0700

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
 import CoinOverview from "@/components/home/CoinOverview";
 import TrendingCoins from "@/components/home/TrendingCoins";
-import { CoinOverviewFallback, TrendingCoinsFallback } from "@/components/home/fallback";
+import { CategoriesFallback, CoinOverviewFallback, TrendingCoinsFallback } from "@/components/home/fallback";
 import Categories from "@/components/home/Categories";
   
 const Page = async () => {
@@ -18,7 +18,7 @@ const Page = async () => {
       </section>
 
       <section className="w-full mt-7 space-y-4">
-        <Suspense fallback={<div>Loading categories...</div>}>
+        <Suspense fallback={<CategoriesFallback />}>
           <Categories />
         </Suspense>
       </section>
